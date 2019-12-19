01735f930ddaba0d8a0946311002e4a51d3c00f2
Curtis <Curtisnortham@hotmail.com>
2019-12-19 22:38:53 +0000

    First Commit
    Added header and 2 sections

---
+++ Index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+
+<link href="libary/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="js/scipt.js" rel="scripts">
+
+<head>
+    <title>atcom - the travel engine</title>
+
+    
+
+
+
+</head>
+
+<link href="css/main.css" rel="stylesheet">
+
+<?php
+include("includes/header.php");
+?>
+
+<main id="main">
+
+    <!----
+     title
+     ----->
+<section id="title">
+    <div class="banner">
+        <div id="image">
+            <img src="img/title_cover.png" alt="">
+        </div>
+        <div class="banner-text">choose<br>clarity not<br><a id="highlight">complexity...</a></div>
+    </div>
+</section>
+
+    <!----
+     quick info
+     ----->
+<section id="info">
+    <div class="con">
+        <div class="info-text">Atcom is a high performance reservation and<br>e-commerce platform for the<a id="highlight"> leisure travel industry.</a></div>
+    </div>
+</section>
+</main>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header id="header">
+    <div class="container">
+        <div id="case" >
+            <a href="" class="logo"><img src="img/logo.png" alt="" title=""></a>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#solutions">Solutions</a></li>
+                    <li><a href="#travel">Travel Sectors</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#careers">Careers</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
